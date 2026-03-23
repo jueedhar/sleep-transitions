08899f1ea594519c1d71b87f6a76b314e0852f4b
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2026-03-23 11:43:00 +0100

    Changed config.py

--- config.py
+++ config.py
@@ -12,6 +12,8 @@ PROJECTROOT = open(".cw", "r").read().rstrip()
 DATA = os.path.join(PROJECTROOT, "Data")
 FIGURES = os.path.join(PROJECTROOT, "Figures")
 
+MASTER_DATA_SHEET = os.path.join(config.DATA, "baboon_sleep_wake_transitions.parquet")
+
 formats=['png', 'pdf', 'svg']
 
 #Miscellaneous

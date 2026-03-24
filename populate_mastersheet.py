4dbe878e61ea9147e8d8812707377bbe7b274245
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2026-03-24 14:42:49 +0100

    Fixed populate_mastersheet with config.py access

--- populate_mastersheet.py
+++ populate_mastersheet.py
@@ -1,81 +1,89 @@
 import os
+
 import pandas as pd 
 import pyarrow as pa
+
+import config
 
 '''
 This creates a master data sheet from 4 different data sheets, the MRBP Mpala Kenya reference data.csv, cluster_labels.csv and 
 individual_night_locations.csv, combine_sleep_analysis.csv, later two of which are updated daily.
 '''
+
+def generate_master_sheet():
 # where the script is located
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = config.PROJECTROOT
 
 # Data directory (must be a folder named "Data" next to the script)
-DATA_DIR = os.path.join(BASE_DIR, "Data")
+    DATA_DIR = config.DATA
 # Input files
-sleep_df = pd.read_csv(
-    os.path.join(DATA_DIR, 'combined_sleep_analysis.csv'),
-    usecols=['tag', 'night_date', 'onset', 'waking']
-)
+    sleep_df = pd.read_csv(
+        os.path.join(DATA_DIR, 'combined_sleep_analysis.csv'),
+        usecols=['tag', 'night_date', 'onset', 'waking']
+    )
 
 # Rename 'tag' to 'animal_id' for consistent merging
-sleep_df.rename(columns={'tag': 'animal_id'}, inplace=True)
+    sleep_df.rename(columns={'tag': 'animal_id'}, inplace=True)
 
-locations_df = pd.read_csv(
-    DATA_DIR, 'individual_night_locations.csv',
-    usecols=['animal_id', 'cluster_united', 'group_id']
-)
+    locations_df = pd.read_csv(
+        os.path.join(DATA_DIR, 'individual_night_locations.csv'),
+        usecols=['animal_id', 'cluster_united', 'group_id']
+    )
 
 # Merge sleep with locations on 'animal_id'
-merged_df = sleep_df.merge(
-    locations_df,
-    on='animal_id',
-    how='left'
-)
+    merged_df = sleep_df.merge(
+        locations_df,
+        on='animal_id',
+        how='left'
+    )
 
 # Load cluster labels  to add sleep_site_type
-cluster_labels_df = pd.read_csv(
-    os.path.join(DATA_DIR, 'cluster_labels.csv'),
-    usecols=['cluster_united', 'sleep_site_type']
-)
+    cluster_labels_df = pd.read_csv(
+        os.path.join(DATA_DIR, 'cluster_labels.csv'),
+        usecols=['cluster_united', 'sleep_site_type']
+    )
 
-merged_df = merged_df.merge(
-    cluster_labels_df,
-    on='cluster_united',
-    how='left'
-)
+    merged_df = merged_df.merge(
+        cluster_labels_df,
+        on='cluster_united',
+        how='left'
+    )
 
-reference_df = pd.read_csv(
-    os.path.join(DATA_DIR, 'Baboons-MBRP-Mpala-Kenya-reference-data.csv'),
-    usecols=['animal-id', 'animal-comments', 'animal-sex']
-)
-reference_df.rename(columns={'animal-id': 'animal_id'}, inplace=True)
+    reference_df = pd.read_csv(
+        os.path.join(DATA_DIR, 'Baboons-MBRP-Mpala-Kenya-reference-data.csv'),
+        usecols=['animal-id', 'animal-comments', 'animal-sex']
+    )
+    reference_df.rename(columns={'animal-id': 'animal_id'}, inplace=True)
 
 
-final_df = merged_df.merge(
-    reference_df,
-    on='animal_id',  # now all merges use the same column
-    how='left'
-)
+    final_df = merged_df.merge(
+        reference_df,
+        on='animal_id',  # now all merges use the same column
+        how='left'
+    )
 # Rename columns 
-final_df.rename(columns={
-    'animal_id': 'ind',
-    'night_date' : 'date',
-    'onset': 't_sleep',
-    'waking': 't_wake',
-    'animal-comments':'age',
-    'animal-sex': 'sex',
-    'cluster_united': 'clutch_id',
-}, inplace=True)
+    final_df.rename(columns={
+        'animal_id': 'ind',
+        'night_date' : 'date',
+        'onset': 't_sleep',
+        'waking': 't_wake',
+        'animal-comments':'age',
+        'animal-sex': 'sex',
+        'cluster_united': 'clutch_id',
+    }, inplace=True)
+
+    return final_df
 
 # Save
-final_df.to_parquet(
-    os.path.join(DATA_DIR, 'masterdata.parquet'),
-    index=False
-)
+def save_master_df(df):
+    df.to_parquet(
+        config.MASTER_DATA_SHEET,
+        index=False
+    )
 
-final_df.to_csv(
-    os.path.join(DATA_DIR, 'masterdata.csv'),
-    index=False
-)
 
-print("Merged metadata saved as masterdata.parquet and masterdata.csv")
+
+
+if __name__ == "__main__":
+    master_df = generate_master_sheet()
+    save_master_df(master_df)
